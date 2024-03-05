1695d6d0480bd6fb818ad20d9103e2f81965562a
joowon_lee <joowon_lee@tmax.co.kr>
2024-03-05 18:16:00 +0900

    parallelTest

---
+++ parallelStream/ParallelStreamTest.java
@@ -0,0 +1,22 @@
+package stream_practice.StreamPractice.parallelStream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ParallelStreamTest {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        long start = System.currentTimeMillis();
+
+        numbers.parallelStream().forEach(number -> {
+            try{
+                Thread.sleep(3000);
+                System.out.println(number + " : " + Thread.currentThread().getName());
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " seconds");
+    }
+}
